893352f3dd701d163ba0d10b129332c17b2a31a9
Markus Bille <43919485+MaxusTheOne@users.noreply.github.com>
2023-09-15 09:05:17 +0200

    fixed my f up

--- database.js
+++ database.js
@@ -2,11 +2,11 @@ import mysql from "mysql2";
 import "dotenv/config";
 
 const connection = mysql.createConnection({
-  host: process.config.env.MYSQL_HOST,
-  port: process.config.env.MYSQL_PORT,
-  user: process.config.env.MYSQL_USER,
-  database: process.config.env.MYSQL_DATABASE,
-  password: process.config.env.MYSQL_PASSWORD,
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  database: process.env.MYSQL_DATABASE,
+  password: process.env.MYSQL_PASSWORD,
   multipleStatements: true,
 });
 
