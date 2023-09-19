27261bd4579f084fb0a8ddd374a4310cb7334175
Benjamin-Harris1 <beha1503@gmail.com>
2023-09-19 15:05:14 +0200

    database fis

--- database.js
+++ database.js
@@ -1,13 +1,27 @@
 import mysql from "mysql2";
 import "dotenv/config";
+import fs from "fs";
 
-const connection = mysql.createConnection({
+// const connection = mysql.createConnection({
+//   host: process.env.MYSQL_HOST,
+//   port: process.env.MYSQL_PORT,
+//   user: process.env.MYSQL_USER,
+//   database: process.env.MYSQL_DATABASE,
+//   password: process.env.MYSQL_PASSWORD,
+//   multipleStatements: true,
+// });
+
+const dbconfig = {
   host: process.env.MYSQL_HOST,
-  port: process.env.MYSQL_PORT,
   user: process.env.MYSQL_USER,
   database: process.env.MYSQL_DATABASE,
   password: process.env.MYSQL_PASSWORD,
-  multipleStatements: true,
-});
+};
+
+if (process.env.MYSQL_CERT) {
+  dbconfig.ssl = { cs: fs.readFileSync("DigiCertGlobalRootCA.crt.pem") };
+}
+
+const connection = mysql.createConnection(dbconfig);
 
 export { connection };
