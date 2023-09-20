6e666d02d57244f531e4efe7880f04d2bdb11936
gustavwiese <gusse12345@gmail.com>
2023-09-20 12:35:02 +0200

    added new start script and udkommenterede i database.js

--- database.js
+++ database.js
@@ -2,26 +2,26 @@ import mysql from "mysql2";
 import "dotenv/config";
 import fs from "fs";
 
-// const connection = mysql.createConnection({
-//   host: process.env.MYSQL_HOST,
-//   port: process.env.MYSQL_PORT,
-//   user: process.env.MYSQL_USER,
-//   database: process.env.MYSQL_DATABASE,
-//   password: process.env.MYSQL_PASSWORD,
-//   multipleStatements: true,
-// });
-
-const dbconfig = {
+const connection = mysql.createConnection({
   host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
   user: process.env.MYSQL_USER,
   database: process.env.MYSQL_DATABASE,
   password: process.env.MYSQL_PASSWORD,
-};
+  multipleStatements: true,
+});
 
-if (process.env.MYSQL_CERT) {
-  dbconfig.ssl = { cs: fs.readFileSync("DigiCertGlobalRootCA.crt.pem") };
-}
+// const dbconfig = {
+//   host: process.env.MYSQL_HOST,
+//   user: process.env.MYSQL_USER,
+//   database: process.env.MYSQL_DATABASE,
+//   password: process.env.MYSQL_PASSWORD,
+// };
 
-const connection = mysql.createConnection(dbconfig);
+// if (process.env.MYSQL_CERT) {
+//   dbconfig.ssl = { cs: fs.readFileSync("DigiCertGlobalRootCA.crt.pem") };
+// }
+
+// const connection = mysql.createConnection(dbconfig);
 
 export { connection };
