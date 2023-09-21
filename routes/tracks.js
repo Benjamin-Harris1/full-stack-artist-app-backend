b3c0a9f6f9bd2231041ee6fca9e566353d9b6e9c
gustavwiese <gusse12345@gmail.com>
2023-09-21 14:43:52 +0200

    post+put tracks works

--- routes/tracks.js
+++ routes/tracks.js
@@ -6,7 +6,7 @@ const tracksRouter = Router();
 // SKAL LAVES TIL mysql/promises SYNTAX LIGESOM DER ER GJORT I ARTISTS.JS
 
 tracksRouter.get("/", async (request, response) => {
-  const query = /*sql*/ `SELECT * FROM tracks ORDER BY title;`;
+  const query = "SELECT * FROM tracks ORDER BY title;";
   const [results] = await dbconfig.execute(query);
   response.json(results);
 });
@@ -19,32 +19,21 @@ tracksRouter.get("/:id", async (request, response) => {
   response.json(results);
 });
 
-tracksRouter.post("/", (request, response) => {
+tracksRouter.post("/", async (request, response) => {
   const tracks = request.body;
   const query = "INSERT INTO tracks (title, duration) VALUES (?, ?); ";
   const values = [tracks.title, tracks.duration];
-  connection.query(query, values, (error, results, fields) => {
-    if (error) {
-      console.log(error);
-    } else {
-      response.json(results);
-    }
-  });
+  const [results] = await dbconfig.execute(query, values);
+  response.json(results);
 });
 
-tracksRouter.put("/:id", (request, response) => {
+tracksRouter.put("/:id", async (request, response) => {
   const id = request.params.id;
   const track = request.body;
   const query = "UPDATE tracks SET title=?, duration=? WHERE id=?;";
   const values = [track.title, track.duration, id];
-
-  connection.query(query, values, (error, results, fields) => {
-    if (error) {
-      console.log(error);
-    } else {
-      response.json(results);
-    }
-  });
+  const [results] = await dbconfig.execute(query, values);
+  response.json(results);
 });
 
 tracksRouter.delete("/:id", (request, response) => {
