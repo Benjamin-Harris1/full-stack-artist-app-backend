c311e8beb3a3af6b5359950c67e39c1208b2806e
Markus Bille <43919485+MaxusTheOne@users.noreply.github.com>
2023-09-22 12:31:29 +0200

    attempted fixing of fullAlbum
    still not done

--- routes/fullAlbum.js
+++ routes/fullAlbum.js
@@ -3,40 +3,37 @@ import { dbconfig } from "../database.js";
 
 const fullAlbumRouter = Router();
 
+/*  inserts an artist, an album and a track and associates them in junction tables
+     Format: {title:string, release_date:string, name:string, career_start:string}
+    Planned*/
 fullAlbumRouter.post("/", async (request, response) => {
-  const album = request.body;
+  const body = request.body;
 
   const albumQuery = /*SQL*/ `
   INSERT INTO albums (title, release_date)
   VALUES (?, ?);`;
-  const albumValues = [album.title, album.release_date];
+  const albumValues = [body.album_title, body.album_release_date];
   const [albumResult] = await dbconfig.execute(albumQuery, albumValues);
 
   const artistQuery = "INSERT INTO artists (name, career_start) VALUES (?, ?);";
-  const artistValues = [artist.name, artist.career_start];
+  const artistValues = [body.artist_name, body.artist_career_start];
   const [artistResults] = await dbconfig.execute(artistQuery, artistValues);
 
   const tracksQuery = "INSERT INTO tracks (title, duration) VALUES (?, ?);";
-  const tracksValues = [tracks.title, tracks.duration];
+  const tracksValues = [body.track_title, body.track_duration];
   const [tracksResults] = await dbconfig.execute(tracksQuery, tracksValues);
 
   const albums_artistsQuery = /*SQL*/ `
   INSERT INTO albums_artists (album_id, artist_id)
   VALUES (?, ?);`;
   const albums_artistsValues = [albumResult.insertId, album.artist_id];
-  const [albums_artistsResult] = await dbconfig.execute(
-    albums_artistsQuery,
-    albums_artistsValues
-  );
+  const [albums_artistsResult] = await dbconfig.execute(albums_artistsQuery, albums_artistsValues);
 
   const tracks_albumsQuery = /*SQL*/ `
   INSERT INTO tracks_albums (track_id, album_id)
   VALUES (?, ?);`;
   const tracks_albumsValues = [tracksResults.insertId, tracks.album_id];
-  const [tracks_albumsResult] = await dbconfig.execute(
-    tracks_albumsQuery,
-    tracks_albumsValues
-  );
+  const [tracks_albumsResult] = await dbconfig.execute(tracks_albumsQuery, tracks_albumsValues);
 
   const query = /*SQL*/ `
  SELECT tracks.title as trackTitle, albums.title as albumTitle, artists.name as artistTitle
