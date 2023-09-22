e8fd061c7e5d184b8a9067b99533a6d35fdf63d4
fili0727 <123998888+fili0727@users.noreply.github.com>
2023-09-22 12:11:17 +0200

    full album route

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import artistsRouter from "./routes/artists.js";
 import albumsRouter from "./routes/albums.js";
 import tracksRouter from "./routes/tracks.js";
+import fullAlbumRouter from "./routes/fullalbum.js";
 
 const app = express();
 const port = process.env.port || 3000;
@@ -10,9 +11,11 @@ const port = process.env.port || 3000;
 app.use(express.json());
 app.use(cors());
 // ROUTERS
+
 app.use("/artists", artistsRouter);
 app.use("/albums", albumsRouter);
 app.use("/tracks", tracksRouter);
+app.use("/fullAlbums", fullAlbumRouter);
 
 app.get("/", (request, response) => {
   response.send("test");
---
+++ routes/fullAlbum.js
@@ -0,0 +1,55 @@
+import { Router, request } from "express";
+import { dbconfig } from "../database.js";
+
+const fullAlbumRouter = Router();
+
+fullAlbumRouter.post("/", async (request, response) => {
+  const album = request.body;
+
+  const albumQuery = /*SQL*/ `
+  INSERT INTO albums (title, release_date)
+  VALUES (?, ?);`;
+  const albumValues = [album.title, album.release_date];
+  const [albumResult] = await dbconfig.execute(albumQuery, albumValues);
+
+  const artistQuery = "INSERT INTO artists (name, career_start) VALUES (?, ?);";
+  const artistValues = [artist.name, artist.career_start];
+  const [artistResults] = await dbconfig.execute(artistQuery, artistValues);
+
+  const tracksQuery = "INSERT INTO tracks (title, duration) VALUES (?, ?);";
+  const tracksValues = [tracks.title, tracks.duration];
+  const [tracksResults] = await dbconfig.execute(tracksQuery, tracksValues);
+
+  const albums_artistsQuery = /*SQL*/ `
+  INSERT INTO albums_artists (album_id, artist_id)
+  VALUES (?, ?);`;
+  const albums_artistsValues = [albumResult.insertId, album.artist_id];
+  const [albums_artistsResult] = await dbconfig.execute(
+    albums_artistsQuery,
+    albums_artistsValues
+  );
+
+  const tracks_albumsQuery = /*SQL*/ `
+  INSERT INTO tracks_albums (track_id, album_id)
+  VALUES (?, ?);`;
+  const tracks_albumsValues = [tracksResults.insertId, tracks.album_id];
+  const [tracks_albumsResult] = await dbconfig.execute(
+    tracks_albumsQuery,
+    tracks_albumsValues
+  );
+
+  const query = /*SQL*/ `
+ SELECT tracks.title as trackTitle, albums.title as albumTitle, artists.name as artistTitle
+  FROM tracks
+  INNER JOIN tracks_albums ON tracks.id = tracks_albums.track_id
+  INNER JOIN albums ON tracks_albums.album_id = albums.id
+  INNER JOIN tracks_artists ON tracks.id = tracks_artists.track_id
+  INNER JOIN artists ON tracks_artists.artist_id = artists.id
+  WHERE tracks.id = ?;`;
+  const values = [tracksResults.insertId];
+  const [result] = await dbconfig.execute(query, values);
+
+  response.json(result[0]);
+});
+
+export default fullAlbumRouter;
